17ba16e1f0d41706d3e72c47a2c61cd2b63cd82d
Kyle ROUTLEDGE <kroutled@e4r7p8.wethinkcode.co.za>
2018-10-10 16:21:14 +0200

    darksky api now working

--- d07/ex00/ex00/ViewController.swift
+++ d07/ex00/ex00/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import RecastAI
+import ForecastIO
 
 class ViewController: UIViewController {
 
     var bot: RecastAIClient? = nil
+     let client = DarkSkyClient(apiKey: "a6bda0c209b0daab9e03fdad6d65caef")
     
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var userInput: UITextField!
@@ -19,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.bot = RecastAIClient(token: "ed6e3290f37e1baabdbe3036858aedda")
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,20 +36,46 @@ class ViewController: UIViewController {
     func makeRequest(request: String)
     {
         //Call makeRequest with string parameter to make a text request
-        self.bot?.textRequest(request, successHandler: {(Response) in self.recastRequestDone(Response)}, failureHandle: {(Response) in self.recastRequestError(Response as! Response)})
+        self.bot?.textRequest(request, successHandler: recastRequestDone(_:), failureHandle: recastRequestError(_:))
+        
+        
+       
     }
     
     func recastRequestDone(_ response : Response)
     {
-        let intent = response.intent()
+        let location = response.get(entity: "location")
+        print(location ?? "No Location")
+        if let myLat = location?["lat"]{
+            let myLon = location!["lng"]
+            
+            self.client.getForecast(latitude: myLat as! Double, longitude: myLon as! Double) { result in
+                switch result {
+                case .success(let currentForecast, let requestMetadata):
+                //  We got the current forecast!
+                    print(currentForecast)
+                    DispatchQueue.main.async {
+                    self.displayLabel.text = currentForecast.currently?.summary
+                    }
+                    break
+                case .failure(let error):
+                    //  Uh-oh. We have an error!
+                    print(error)
+                    self.displayLabel.text = "Error"
+                    break
+                }
+            }
         //print(intent?.slug)
-        displayLabel.text = intent?.slug
+//        displayLabel.text = intent?.slug
+        }
+        else{
+            self.displayLabel.text = "No such location"
+        }
     }
     
-    func recastRequestError(_ response : Response)
+    func recastRequestError(_ error: Error)
     {
         
     }
-
 }
 
